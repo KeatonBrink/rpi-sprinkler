1e8162acc14e03290720658327307e491ee5b4e3
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-07-04 19:16:04 -0600

    Server serves files

---
+++ middle_server_method/go_server/go.mod
@@ -0,0 +1,5 @@
+module github.com/KeatonBrink/rpi-sprinkler/tree/main/middle_server_method/go_server
+
+go 1.22.5
+
+require github.com/KeatonBrink/gologger v0.0.0-20240625193908-d362f711b81b // indirect
--- middle_server_method/go_server/main.go
+++ middle_server_method/go_server/main.go
@@ -1,16 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/KeatonBrink/gologger"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type State struct {
+	// some state
+	isSprinklerOn bool
+	currentLogs   []string
 }
 
+var s State
+
+// type page struct {
+// 	Title string
+// 	Body  []byte
+// }
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	type Status struct {
+		IsSprinklerOn bool `json:"isSprinklerOn"`
+	}
+
+	status := Status{IsSprinklerOn: s.isSprinklerOn}
+	statusJson, err := json.Marshal(status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		gologger.QueueMessage("Error marshalling state to JSON")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(statusJson)
+}
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
 func main() {
-	http.HandleFunc("/", handler)
+	// err := gologger.InitGoLogger(true, "output.log")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	go gologger.EmptyMessageQueue()
+
+	s = State{isSprinklerOn: false, currentLogs: []string{}}
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/status", statusHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
